4098f6df745ede6dbd45da8cc55f26a036d4b494
Nazih Kalo <49926988+nazihkalo@users.noreply.github.com>
2022-07-01 17:05:49 -0700

    testing new click requirement

--- cli/setup.py
+++ cli/setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         "base58",
         "blockchain-etl-common==1.6.1",
-        "click==7.0",
+        "click==8.0.4",
         "eth-abi==1.3.0",
         "eth-utils==1.8.4",
         "ethereum-dasm==0.1.4",
